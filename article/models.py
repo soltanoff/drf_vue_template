30b72517749e02d6a146ecf00578d6229b39d9df
Ilya Soltanov <i_soltanov@wargaming.net>
2018-12-06 13:26:56 +0300

    changed db field type: `int` -> `int` with auto

--- article/models.py
+++ article/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class ArticleModel(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     title = models.CharField(name='Title', max_length=250)
     content = models.TextField(name='Content')
