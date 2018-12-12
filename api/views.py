d6c06108a6c6801316be7eecad8f94bfef020545
Ilya <piccadillable@gmail.com>
2018-12-12 11:05:17 +0300

    added queryset filter by GET params

--- api/views.py
+++ api/views.py
@@ -7,3 +7,10 @@ from article.models import ArticleModel
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = ArticleModel.objects.all()
     serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = super(ArticleViewSet, self).get_queryset()
+        search_name = self.request.query_params.get('search', None)
+        if search_name is not None:
+            queryset = queryset.filter(title__contains=search_name)
+        return queryset
