f44abc7fddf1a9d04af74daa1b8e4d88a5b00c60
Ilya <piccadillable@gmail.com>
2018-12-17 14:42:29 +0300

    #2: added custom paginator for article list

--- api/views.py
+++ api/views.py
@@ -1,12 +1,38 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from api.serilizers import ArticleSerializer
 from article.models import ArticleModel
 
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 1
+    max_page_size = 15
+
+    def get_url(self, page_number):
+        query_params = filter(lambda (key, _): key != self.page_query_param, self.request.query_params.items())
+        query_params.append((self.page_query_param, page_number))
+        return '&'.join(map(lambda x: '%s=%s' % x, query_params))
+
+    def get_next_link(self):
+        if not self.page.has_next():
+            return None
+        url = self.request.get_full_path()
+        page_number = self.page.next_page_number()
+        return '%s?%s' % (url, self.get_url(page_number))
+
+    def get_previous_link(self):
+        if not self.page.has_previous():
+            return None
+        url = self.request.get_full_path()
+        page_number = self.page.previous_page_number()
+        return '%s?%s' % (url, self.get_url(page_number))
+
+
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = ArticleModel.objects.all()
     serializer_class = ArticleSerializer
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         queryset = super(ArticleViewSet, self).get_queryset()
--- article/models.py
+++ article/models.py
@@ -5,3 +5,6 @@ class ArticleModel(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(name='title', max_length=250)
     content = models.TextField(name='content')
+
+    class Meta:
+       ordering = ['id']
