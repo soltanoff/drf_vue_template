e26adfdfedfae19158ce6d85b79393d41777983e
soltanoff <piccadillable@gmail.com>
2019-07-04 18:22:33 +0300

    :octocat: reformat Vue.js code

--- article/static/js/main.js
+++ article/static/js/main.js
@@ -1,92 +1,95 @@
 new Vue({
-  el: '#app',
-  delimiters: ['${','}'],
-  data: {
-    articles: [],
-    loading: true,
-    preview: false,
-    currentArticle: {},
-    message: null,
-    newArticle: { 'title': '', 'content': '' },
-    search_term: '',
-  },
-  mounted: function() {
-    this.getArticles();
-  },
-  methods: {
-    getArticles: function(useLoading=true, page=1) {
-      let api_url = '/api/article/';
-      if(this.search_term!==''||this.search_term!==null) {
-        api_url = `/api/article/?search=${this.search_term}&page=${page}`
-      }
-      this.loading = useLoading;
-      this.$http.get(api_url)
-          .then((response) => {
-            this.articles = response.data['results'];
-            this.loading = false;
-          })
-          .catch((err) => {
-            this.loading = false;
-            console.log(err);
-          })
+    el: '#app',
+    delimiters: ['${', '}'],
+    data: {
+        loading: true,
+        preview: false,
+        
+        articles: [],
+        currentArticle: {},
+        newArticleTemplate: {'title': '', 'content': ''},
+        
+        search_term: '',
     },
-    getArticle: function(id) {
-      this.$http.get(`/api/article/${id}/`)
-          .then((response) => {
-            this.currentArticle = response.data;
-            $("#editArticleModal").modal('show');
-          })
-          .catch((err) => {
-            console.log(err);
-          })
+    mounted: function() {
+        this.getArticles();
     },
-    showArticle: function(id) {
-      this.$http.get(`/api/article/${id}/`)
-          .then((response) => {
-            this.currentArticle = response.data;
-            this.preview = true;
-          })
-          .catch((err) => {
-            this.preview = false;
-            console.log(err);
-          })
-    },
-    addArticle: function() {
-      this.loading = true;
-      this.$http.post('/api/article/',this.newArticle)
-          .then((response) => {
+    methods: {
+        getArticles: function(withLoading=true) {
+            this.loading = withLoading;
+            this.$http
+                .get(`/api/article/`)
+                .then((response) => {
+                    this.articles = response.data;
+                    this.loading = false;
+                })
+                .catch((error) => {
+                    this.loading = false;
+                    console.log(error);
+                })
+        },
+        getArticle: function(id) {
+            this.$http
+                .get(`/api/article/${id}/`)
+                .then((response) => {
+                    this.currentArticle = response.data;
+                    $("#editArticleModal").modal('show');
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        },
+        showArticle: function(id) {
+            this.$http
+                .get(`/api/article/${id}/`)
+                .then((response) => {
+                    this.currentArticle = response.data;
+                    this.preview = true;
+                })
+                .catch((error) => {
+                    this.preview = false;
+                    console.log(error);
+                })
+        },
+        addArticle: function() {
             this.loading = true;
-            this.getArticles();
-          })
-          .catch((err) => {
-            this.loading = true;
-            console.log(err);
-          })
-    },
-    updateArticle: function() {
-      this.loading = true;
-      this.$http.put(`/api/article/${this.currentArticle.id}/`, this.currentArticle)
-          .then((response) => {
-            this.loading = false;
-            this.currentArticle = response.data;
-            this.getArticles();
-          })
-          .catch((err) => {
-            this.loading = false;
-            console.log(err);
-          })
-    },
-    deleteArticle: function(id) {
-      this.loading = true;
-      this.$http.delete(`/api/article/${id}/`)
-          .then((response) => {
-            this.loading = false;
-            this.getArticles();
-          })
-          .catch((err) => {
-            this.loading = false;
-            console.log(err);
-          })
+            this.$http
+                .post(`/api/article/`, this.newArticleTemplate)
+                .then((response) => {
+                    this.loading = true;
+                    this.getArticles();
+                })
+                .catch((error) => {
+                    this.loading = true;
+                    console.log(error);
+                })
+        },
+        updateArticle: function() {
+            // this.loading = true;
+            this.$http
+                .put(`/api/article/${this.currentArticle.id}/`, this.currentArticle)
+                .then((response) => {
+                    this.loading = false;
+                    this.currentArticle = response.data;
+                    this.getArticles();
+                })
+                .catch((error) => {
+                    this.loading = false;
+                    console.log(error);
+                })
+        },
+        deleteArticle: function(id) {
+            // this.loading = true;
+            this.$http
+                .delete(`/api/article/${id}/`)
+                .then((response) => {
+                    this.loading = false;
+                    this.getArticles(withLoading=false);
+                })
+                .catch((error) => {
+                    this.loading = false;
+                    console.log(error);
+                })
+        }
     }
-  }
 });
