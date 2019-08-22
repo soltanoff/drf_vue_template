d3dd8bb70bb883ab1702a086873e86f3dc3a02cc
soltanoff <piccadillable@gmail.com>
2019-08-22 13:31:14 +0300

    Update PageNumberPagination :camel:

--- api/views.py
+++ api/views.py
@@ -9,34 +9,12 @@ from article.models import ArticleModel
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 1
+    page_size = 5
     max_page_size = 15
 
-    def get_url(self, page_number):
-        query_params = list(filter(lambda item: item[0] != self.page_query_param, self.request.query_params.items()))
-        query_params.append((self.page_query_param, page_number))
-        return '&'.join(map(lambda x: '%s=%s' % x, query_params))
-
-    def get_next_link(self):
-        if not self.page.has_next():
-            return None
-        url = self.request.get_full_path()
-        page_number = self.page.next_page_number()
-        return '%s?%s' % (url, self.get_url(page_number))
-
-    def get_previous_link(self):
-        if not self.page.has_previous():
-            return None
-        url = self.request.get_full_path()
-        page_number = self.page.previous_page_number()
-        return '%s?%s' % (url, self.get_url(page_number))
-    
     def get_paginated_response(self, data):
         return Response(OrderedDict([
-            ('page_count', self.page.paginator.count),
-            ('current_page', self.page.number),
-            ('next_page_url', self.get_next_link()),
-            ('previous_page_url', self.get_previous_link()),
+            ('page_count', self.page.paginator.num_pages),
             ('articles', data)
         ]))
 
