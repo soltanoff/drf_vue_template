3f32c0e858b4041afa42035e8d3b38330d77f6c2
soltanoff <piccadillable@gmail.com>
2021-06-02 22:44:40 +0300

    rearrange code

--- api/views.py
+++ api/views.py
@@ -1,28 +1,20 @@
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.utils.translation import gettext_lazy as _
+from django.core.paginator import Paginator, EmptyPage
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination, _get_page_links, _get_displayed_page_numbers
 from rest_framework.response import Response
 from rest_framework.utils.urls import replace_query_param, remove_query_param
 
-from api.serilizers import ArticleSerializer
 from api.models import ArticleModel
+from api.serilizers import ArticleSerializer
 
 
 class SoftDjangoPaginator(Paginator):
     def validate_number(self, number):
         try:
-            if isinstance(number, float) and not number.is_integer():
-                raise ValueError
-            number = int(number)
-        except (TypeError, ValueError):
-            raise PageNotAnInteger(_('That page number is not an integer'))
-        
-        if number < 1:
-            raise EmptyPage(_('That page number is less than 1'))
-        
-        if number > self.num_pages and not (number == 1 and self.allow_empty_first_page):
-            return 1
+            number = super(SoftDjangoPaginator, self).validate_number(number)
+        except EmptyPage:
+            number = 1
+
         return number
 
 
@@ -38,16 +30,13 @@ class StandardResultsSetPagination(PageNumberPagination):
         else:
             return replace_query_param(base_url, self.page_query_param, page_number)
 
-    def get_page_info(self):
+    def get_paginated_response(self, data):
         current = self.page.number
         page_numbers = _get_displayed_page_numbers(current, self.page.paginator.num_pages)
         page_links = _get_page_links(page_numbers, current, self.page_number_to_url)
 
-        return [{'number': number, 'link': link.url } for number, link in zip(page_numbers, page_links)]
-
-    def get_paginated_response(self, data):
         return Response({
-            'pages_info': self.get_page_info(),
+            'pages_info': [{'number': number, 'link': link.url} for number, link in zip(page_numbers, page_links)],
             'articles': data,
         })
 
